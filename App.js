e90daec1d4e8d740c2cb516129b74e1cee8757ff
Guzman Riancho <guzman.gonzalezriancho@alumnos.uneatlantico.es>
2022-08-23 16:43:39 +0200

    1er cambio app

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Esta vivo, ojo, hola borja</Text>
+      <Image
+        source={require('./assets/logo.png')}
+        style={{ width: 100, height: 100  }}
+      />
+      <Text></Text>
+      <Button
+        onPress={() => {
+          alert('Signed In!');
+        }}
+        title="Sign in with Email"
+        color='#5EA780'
+      />
       <StatusBar style="auto" />
     </View>
   );
