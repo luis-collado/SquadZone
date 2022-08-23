fbf68ee5d1cb6dfad7929a24da5d9a2ac0e8ff02
luis-collado <luis.collado.pu@gmail.com>
2022-08-23 16:37:27 +0200

    start app

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image
-        source={require('./assets/logo.png')}
-        style={{ width: 100, height: 100  }}
-      />
-      <Text></Text>
-      <Button
-        onPress={() => {
-          alert('Signed In!');
-        }}
-        title="Sign in with Email"
-        color='#5EA780'
-      />
+      <Text>Esta vivo, ojo, hola borja</Text>
       <StatusBar style="auto" />
     </View>
   );
