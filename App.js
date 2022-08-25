a61e5e7dbdcbfea7faa5f191d50f073be522967f
Guzman Riancho <guzman.gonzalezriancho@alumnos.uneatlantico.es>
2022-08-25 22:41:26 +0200

    añadido varias dependencias y creado 4 pantallas principales con sus botones

--- App.js
+++ App.js
@@ -1,31 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionic from "react-native-vector-icons/Ionicons";
+import {NavigationContainer} from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/components/screens/Home';
+import News from './src/components/screens/News';
+import Search from './src/components/screens/Search';
+import Profile from './src/components/screens/Profile';
 
-export default function App() {
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  const bottomTabScreen = () => {
+    return(
+      <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle:{
+          height:50
+        },
+
+        tabBarIcon: ({focused, size, colour}) => {
+          let iconName;
+          if(route.name === "Home"){
+            iconName= focused ? "home-sharp" : "home-outline";
+            size = focused ? size + 8 : size + 2;
+          }else if(route.name === "News"){
+            iconName= focused ? "megaphone" : "ios-megaphone-outline";
+          }else if(route.name === "Search"){
+            iconName= focused ? "shield" : "ios-shield-outline";
+          }else if(route.name === "Profile"){
+            iconName= focused ? "person" : "ios-person-outline";
+          }
+
+          return <Ionic name={iconName} size={size} color={colour}/>
+        }
+
+      })}>
+        <Tab.Screen name ="Home" component={Home}/>
+        <Tab.Screen name ="News" component={News}/>
+        <Tab.Screen name ="Search" component={Search}/>
+        <Tab.Screen name ="Profile" component={Profile}/>
+
+      </Tab.Navigator>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('./assets/logo.png')}
-        style={{ width: 100, height: 100  }}
-      />
-      <Text></Text>
-      <Button
-        onPress={() => {
-          alert('Signed In!');
-        }}
-        title="Sign in with Email"
-        color='#5EA780'
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Bottom" component={bottomTabScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App;
---
+++ src/components/screens/News.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-context'
+
+const News = () => {
+  return (
+    <SafeAreaView>
+        <View>
+            <Text>News</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default News
